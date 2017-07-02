217e1266699f331f6c21c0c56ba8c69923c392f1
Will Xyen <willxinc@users.noreply.github.com>
2017-07-02 03:56:00 -0400

    finish exporter

--- assets/js/core.js
+++ assets/js/core.js
@@ -9,7 +9,7 @@ for (var i in music_data) {
 
 	var this_entry = $("<tr class=\"song\" num_id=" + entry.id + "></tr>").appendTo("#songs_table");
 
-	this_entry.append("<td><a href=\"https://remywiki.com/index.php?go=Go&search=" + entry.name + "\">" + entry.name + "</a></td>");
+	this_entry.append("<td><a target=\"_blank\" href=\"https://remywiki.com/index.php?go=Go&search=" + entry.name + "\">" + entry.name + "</a></td>");
 
 	var this_fav = $("<td class=\"favs_entry\" num_id=" + entry.id + "></td>").appendTo(this_entry);
 
@@ -31,6 +31,24 @@ for (var i in music_data) {
 			this_fav.append("<input type=\"checkbox\" name=\"" + each_diff + "\" disabled>");
 		}
 	}
+}
+function uint8_to_hex(d) {
+    var s = (+d).toString(16);
+    if(s.length < 2) {
+        s = '0' + s;
+    }
+    return s;
+}
+
+function uint32_to_hex(d) {
+    var s = (+d).toString(16);
+    if(s.length < 8) {
+        s = '0' + s;
+    }
+    var split = s.match(/../g);             // split number in groups of two
+    split.reverse();                        // reverse the groups
+	var s2 = split.join("");                // join the groups back together
+    return s2;
 }
 
 $( "#export_selected" ).click(function() {
@@ -60,6 +78,62 @@ $( "#export_selected" ).click(function() {
 	console.log(sp_diff);
 	console.log(dp_favs);
 	console.log(dp_diff);
+
+	var fav_header = 
+	'<?xml version="1.0" encoding="UTF-8"?>' + 
+		'<IIDX23pc_favs>' + 
+    		'<favorite>';
+
+	var fav_footer = 
+			'</favorite>' + 
+		'</IIDX23pc_favs>';
+
+	var fav_contents = "";
+	fav_contents += '<sp_mlist __type="bin" __size="80">';
+	var insertions = 0;
+	for (var each_sp_fav in sp_favs){
+		fav_contents += uint32_to_hex(sp_favs[each_sp_fav]);
+		++insertions;
+	}
+	for (var remaining = insertions; remaining < 20; remaining++){
+		fav_contents += "00000000";
+	}
+	fav_contents += '</sp_mlist>';
+
+	fav_contents += '<sp_clist __type="bin" __size="20">';
+	var insertions = 0;
+	for (var each_sp_fav in sp_diff){
+		fav_contents += uint8_to_hex(sp_diff[each_sp_fav]);
+		++insertions;
+	}
+	for (var remaining = insertions; remaining < 20; remaining++){
+		fav_contents += "00";
+	}
+	fav_contents += '</sp_clist>';
+
+	fav_contents += '<dp_mlist __type="bin" __size="80">';
+	var insertions = 0;
+	for (var each_dp_fav in dp_favs){
+		fav_contents += uint32_to_hex(sp_favs[each_dp_fav]);
+		++insertions;
+	}
+	for (var remaining = insertions; remaining < 20; remaining++){
+		fav_contents += "00000000";
+	}
+	fav_contents += '</dp_mlist>';
+
+	fav_contents += '<dp_clist __type="bin" __size="20">';
+	var insertions = 0;
+	for (var each_dp_fav in dp_diff){
+		fav_contents += uint8_to_hex(dp_diff[each_dp_fav]);
+		++insertions;
+	}
+	for (var remaining = insertions; remaining < 20; remaining++){
+		fav_contents += "00";
+	}
+	fav_contents += '</dp_clist>';
+
+	$(".xml_data").val(fav_header + fav_contents + fav_footer);
 });
 
 $( "#import_from" ).click(function() {
