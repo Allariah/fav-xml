a7bd3a91b5a6c577c71a7a77b97ea9f1f5700755
Will Xyen <willxinc@users.noreply.github.com>
2017-07-02 02:48:04 -0400

    Fix formatting / js error

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,37 +1,38 @@
 $( "#songs" ).empty();
 $( "#favs" ).empty();
 
-for each(var entry in music_data) {
+for (var entry in music_data) {
 	$( "#songs" ).append("<div class=\"song\" num_id=" + entry.id + ">" + entry.name + "</div>");
+	var this_fav = $( "#favs" ).append("<div class=\"favs_entry\" num_id=" + entry.id + "></div>");
+
 	if (entry.diff_N7 > 0){
-		$( "#favs" ).append("<input type=\"checkbox\" name=\"diff_N7\">");
+		this_fav.append("<input type=\"checkbox\" name=\"diff_N7\">");
 	}else{
-		$( "#favs" ).append("<input type=\"checkbox\" name=\"diff_N7\" disabled>");
+		this_fav.append("<input type=\"checkbox\" name=\"diff_N7\" disabled>");
 	}
 	if (entry.diff_H7 > 0){
-		$( "#favs" ).append("<input type=\"checkbox\" name=\"diff_H7\">");
+		this_fav.append("<input type=\"checkbox\" name=\"diff_H7\">");
 	}else{
-		$( "#favs" ).append("<input type=\"checkbox\" name=\"diff_H7\" disabled>");
+		this_fav.append("<input type=\"checkbox\" name=\"diff_H7\" disabled>");
 	}
 	if (entry.diff_A7 > 0){
-		$( "#favs" ).append("<input type=\"checkbox\" name=\"diff_A7\">");
+		this_fav.append("<input type=\"checkbox\" name=\"diff_A7\">");
 	}else{
-		$( "#favs" ).append("<input type=\"checkbox\" name=\"diff_A7\" disabled>");
+		this_fav.append("<input type=\"checkbox\" name=\"diff_A7\" disabled>");
 	}
 	if (entry.diff_N14 > 0){
-		$( "#favs" ).append("<input type=\"checkbox\" name=\"diff_N14\">");
+		this_fav.append("<input type=\"checkbox\" name=\"diff_N14\">");
 	}else{
-		$( "#favs" ).append("<input type=\"checkbox\" name=\"diff_N14\" disabled>");
+		this_fav.append("<input type=\"checkbox\" name=\"diff_N14\" disabled>");
 	}
 	if (entry.diff_H14 > 0){
-		$( "#favs" ).append("<input type=\"checkbox\" name=\"diff_H14\">");
+		this_fav.append("<input type=\"checkbox\" name=\"diff_H14\">");
 	}else{
-		$( "#favs" ).append("<input type=\"checkbox\" name=\"diff_H14\" disabled>");
+		this_fav.append("<input type=\"checkbox\" name=\"diff_H14\" disabled>");
 	}
 	if (entry.diff_A14 > 0){
-		$( "#favs" ).append("<input type=\"checkbox\" name=\"diff_A14\">");
+		this_fav.append("<input type=\"checkbox\" name=\"diff_A14\">");
 	}else{
-		$( "#favs" ).append("<input type=\"checkbox\" name=\"diff_A14\" disabled>");
+		this_fav.append("<input type=\"checkbox\" name=\"diff_A14\" disabled>");
 	}
-	$( "#favs" ).append("<br/>");
 }
