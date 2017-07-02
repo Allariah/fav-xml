a84d08e8295485134a0062a34a7f77da3029b3f5
Will Xyen <willxinc@users.noreply.github.com>
2017-07-02 03:09:50 -0400

    covnert to table

--- assets/js/core.js
+++ assets/js/core.js
@@ -4,12 +4,24 @@ $( "#favs" ).empty();
 for (var i in music_data) {
 	var entry = music_data[i];
 
-	$( "#songs" ).append("<div class=\"song\" num_id=" + entry.id + ">" + entry.name + "</div>");
-	var this_fav = $( "#favs" ).append("<div class=\"favs_entry\" num_id=" + entry.id + "></div>");
+	var this_entry = $( "#songs_table").append("<tr class=\"song\" num_id=" + entry.id + "></tr>");
 
-	var diffs = ["diff_N7", "diff_H7", "diff_A7", "diff_N14", "diff_H14", "diff_A14"];
+	this_entry.append("<td>" + entry.name + "</td>");
 
-	for (var each_diff in diffs){
+	var this_fav = this_entry.append("<td class=\"favs_entry\" num_id=" + entry.id + "></td>");
+
+	var diffs_7 = ["diff_N7", "diff_H7", "diff_A7"];
+	var diffs_14 = ["diff_N14", "diff_H14", "diff_A14"];
+
+	for (var each_diff in diffs_7){
+		if (entry[each_diff] > 0){
+			this_fav.append("<input type=\"checkbox\" name=\"" + each_diff + "\">");
+		}else{
+			this_fav.append("<input type=\"checkbox\" name=\"" + each_diff + "\" disabled>");
+		}
+	}
+	this_fav.append("<div id=\"space\"> </div>");
+	for (var each_diff in diffs_14){
 		if (entry[each_diff] > 0){
 			this_fav.append("<input type=\"checkbox\" name=\"" + each_diff + "\">");
 		}else{
